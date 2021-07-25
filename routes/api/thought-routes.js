33f64375691b11d0318529e7afd5e49bec2c0a74
bsottek <bsottek@gmail.com>
2021-07-25 17:11:00 -0500

    index and thought-routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById);
+
+router
+    .route('/:thoughtId/:userId')
+    .put(updateThought)
+    .delete(removeThought);
+
+// /api/thoughts/thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .put(addReaction);
+
+// /api/Thoughts/:thoughtId/:reactionId
+router.route('/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
